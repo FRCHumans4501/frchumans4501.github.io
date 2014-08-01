d28e5f671d6f278a981322bf9a6df6ccf7f5557d
Jerish Brown <thejerishbrown@gmail.com>
2014-07-31 17:03:36 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Humans &middot; FRC Team 4501</title>
+  <link href="style/reset.css" rel="stylesheet" type='text/css'/>
+  <link href="style/site.css" rel="stylesheet" type='text/css'/>
+</head>
+
+<body>
+  <header>
+      <div id="logo"></div>
+  </header>
+  <nav>
+    <ul>
+      <li><a href="/about">About</a></li>
+      <li><a href="/team">Team</a></li>
+      <li><a href="/bots">Bots</a></li>
+      <li><a href="/contact">Contact</a></li>
+  </ul>
+  </nav>
+  <main>
+    <article>
+      <h1>Hello World</h1>
+      <p>Hello World! This is our first post on our new website. We have some big plans for the summer and upcoming year. We're gearing up for a complete rework of our team's structure at the same time as preparing for the new year.</p>
+    </article>
+    <article>
+      <h1>Jekyll Explaination</h1>
+      <p>You'll find this post in your `_posts` directory - edit this post and re-build (or run with the `-w` switch) to see your changes!
+To add new posts, simply add a file in the `_posts` directory that follows the convention: YYYY-MM-DD-name-of-post.ext.</p>
+    </article>
+  </main>
+  <footer>
+    
+  </footer>
+</body>
+</html>
